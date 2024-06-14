04511edcb83b4cbf93a536de68e93d1d4653e712
Baseer Rahimi <mohammadbaseer25@gmail.com>
2024-06-14 20:57:28 +0200

    user Endpoint Fixed the issues

--- server/controller/UserController.js
+++ server/controller/UserController.js
@@ -1,29 +1,42 @@
 import colors from "colors";
 import UserModel from "../models/userModel.js";
+import { SecurePassword } from "../utils/Bcrypt/PasswordHash.js";
 
-export const RegisterUser = async (req, res) => {
-  const { name, email, password } = req.body;
+const RegisterUser = async (req, res) => {
   if (!req.file) {
     return res.status(400).json({ error: "No file uploaded" });
   }
-  const photoPath = req.file.path;
-  const user = new UserModel({
-    name,
-    email,
-    password,
-    avatar: photoPath,
-  });
   try {
-    const InsertUser = await user.save();
-    if (InsertUser) {
-      res.status(200).send({
-        Result: InsertUser,
-      });
+    const photoPath = req.file.path;
+    const sPassword = await SecurePassword(req.body.password);
+    const user = new UserModel({
+      name: req.body.name,
+      email: req.body.email,
+      password: sPassword,
+      avatar: photoPath,
+    });
+    const userData = await UserModel.findOne({ email: req.body.email });
+    if (userData) {
+      return res.status(200).send({ error: "user already exists" });
+    } else {
+      const InsertUser = await user.save();
+      res.status(200).send({ Result: InsertUser });
     }
   } catch (error) {
-    res.status(500).send({
+    res.status(400).send(error);
+  }
+};
+
+const UserFetchData = async (req, res) => {
+  try {
+    const userData = await UserModel.find({}, "name email avatar createdAt");
+    res.status(200).json(userData);
+  } catch (error) {
+    res.status(200).send({
+      error: error.message,
       code: error.code,
-      message: error.message,
     });
   }
 };
+
+export { RegisterUser, UserFetchData };

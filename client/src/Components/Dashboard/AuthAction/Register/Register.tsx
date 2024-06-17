bc04afcf7743887b09e6c2102d1d3ebad7b200f3
Baseer Rahimi <mohammadbaseer25@gmail.com>
2024-06-18 00:27:31 +0200

    modified the Registration page

--- client/src/Components/Dashboard/AuthAction/Register/Register.tsx
+++ client/src/Components/Dashboard/AuthAction/Register/Register.tsx
@@ -1,47 +1,51 @@
 // import "./Login.scss";
-import { ChangeEvent, FormEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useEffect, useRef, useState } from "react";
 import styles from "./Register.module.scss";
 import { BaseURL } from "../../../../Utils/URLs/ApiURL";
 import { NotOkType } from "../../../../@Types/Types";
 import avatar from "../../../../assets/img/registrationFormAvatar/addAvatar.png";
 
 const Register = () => {
-  const [image, setImage] = useState<any>(null);
+  const selectedFile = useRef<File | null>(null);
+  const [previewImg, setPreviewImg] = useState<string | null>(null);
   const [errorHandler, setErrorHandler] = useState<NotOkType | string | any>("");
-  const [newUser, setNewUser] = useState({ name: "", email: "", password: "", avatar: null as File | null });
-
-  const getInputValues = (e: ChangeEvent<HTMLInputElement>) => {
-    const { name, value, files } = e.target;
-
-    if (files && files.length > 0) {
-      setNewUser({ ...newUser, [name]: files[0] });
-    } else {
-      setNewUser({ ...newUser, [name]: value });
-    }
-    if (e.target.files && e.target.files[0]) {
-      setImage(URL.createObjectURL(e.target.files[0]));
-    }
-  };
+  const [newUser, setNewUser] = useState({ name: "", email: "", password: "" });
 
   const UserRegisterFun = async (e: FormEvent<HTMLFormElement>) => {
     setErrorHandler("");
     e.preventDefault();
+
+    if (!newUser.name.trim()) {
+      setErrorHandler("Username is missing");
+      return;
+    }
+    if (!newUser.email.trim()) {
+      setErrorHandler("Email is missing");
+      return;
+    }
+    if (!newUser.password.trim()) {
+      setErrorHandler("Password is missing");
+      return;
+    }
 
     const formdata = new FormData();
     formdata.append("name", newUser.name);
     formdata.append("email", newUser.email);
     formdata.append("password", newUser.password);
 
-    if (newUser.avatar) {
-      formdata.append("avatar", newUser.avatar);
+    if (selectedFile.current) {
+      formdata.append("avatar", selectedFile.current);
     }
-
     try {
       const response = await fetch(`${BaseURL}/auth/user`, { method: "POST", body: formdata });
+
+      console.log("response", response);
       if (response.ok) {
         const data = await response.json();
         console.log("Result ===> ", data);
-        setNewUser({ name: "", email: "", password: "", avatar: null });
+        setNewUser({ name: "", email: "", password: "" });
+        selectedFile.current = null;
+        setPreviewImg(null);
       }
       if (!response.ok) {
         const data = (await response.json()) as NotOkType;
@@ -53,6 +57,38 @@ const Register = () => {
       console.log("err==============>", error);
     }
   };
+
+  const getInputValues = (e: ChangeEvent<HTMLInputElement>) => {
+    setNewUser((prev) => {
+      return { ...prev, [e.target.name]: e.target.value };
+    });
+  };
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.files);
+    if (previewImg) {
+      URL.revokeObjectURL(previewImg);
+    }
+    if (e.target.files && e.target.files.length > 0) {
+      selectedFile.current = e.target.files[0];
+      const tempURL = URL.createObjectURL(e.target.files[0]);
+      setPreviewImg(tempURL);
+    } else {
+      if (previewImg) {
+        URL.revokeObjectURL(previewImg);
+      }
+    }
+  };
+
+  console.log("previewImg=====>", previewImg);
+
+  useEffect(() => {
+    return () => {
+      if (previewImg) {
+        URL.revokeObjectURL(previewImg);
+      }
+    };
+  }, []);
 
   return (
     <div className={styles.container}>
@@ -79,13 +115,13 @@ const Register = () => {
 
           <div>
             <label className={styles.file} htmlFor="file">
-              <img className={styles.avatar} src={image === null ? avatar : image} alt="" />
+              <img className={styles.avatar} src={previewImg === null ? avatar : previewImg} alt="preview of selected file" />
               <span>Add an avatar</span>
             </label>
-            <input style={{ display: "none" }} type="file" id="file" name="avatar" onChange={getInputValues} />
+            <input style={{ display: "none" }} type="file" id="file" name="avatar" onChange={handleFileChange} />
           </div>
-
-          {errorHandler && <div className={styles.error}>{typeof errorHandler === "string" ? errorHandler : errorHandler.error}</div>}
+          {/* {errorHandler && <div className={styles.error}>{typeof errorHandler === "string" ? errorHandler : errorHandler.error}</div>} */}
+          <div className={styles.error}>{errorHandler}</div>
 
           <div className={styles.sub_btn_box}>
             <button className={styles.form_btn} type="submit">

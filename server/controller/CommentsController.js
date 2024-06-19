c946eb27ee49992cd788258a0a07a6de3fda32e9
Baseer Rahimi <mohammadbaseer25@gmail.com>
2024-06-19 13:42:09 +0200

    Comment Delete End-Point Created

--- server/controller/CommentsController.js
+++ server/controller/CommentsController.js
@@ -1,10 +1,10 @@
 import CommentModel from "../models/CommentsModel.js";
 
+//! Submit Comment APi Endpoint
 const insertComments = async (req, res) => {
   if (!req.body.comment) {
     return res.status(400).json({ error: "cannot submit the blank comment" });
   }
-
   try {
     const insertNewComment = new CommentModel(req.body);
     await insertNewComment.save();
@@ -14,4 +14,15 @@ const insertComments = async (req, res) => {
   }
 };
 
-export default insertComments;
+//! Delete Comment APi Endpoint
+const deleteComments = async (req, res) => {
+  const commentID = req.params.id;
+  try {
+    await CommentModel.findByIdAndDelete({ _id: commentID });
+    res.status(200).json({ msg: "Comment deleted" });
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+};
+
+export { insertComments, deleteComments };
--- server/routes/commentsRoute.js
+++ server/routes/commentsRoute.js
@@ -1,9 +1,12 @@
 import express from "express";
-import insertComments from "../controller/CommentsController.js";
+import { deleteComments, insertComments } from "../controller/CommentsController.js";
 
 const CommentRoute = express.Router();
 
 //! Insert comment Route API
 CommentRoute.post("/comments", insertComments);
+
+//! Insert comment Route API
+CommentRoute.get("/comments/:id", deleteComments);
 
 export { CommentRoute };

8f4beb187cee8e730d0cfd28db6ab2fdf8fb504e
Baseer Rahimi <mohammadbaseer25@gmail.com>
2024-06-21 01:32:01 +0200

    removed the comment and changed the get to post

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -2,26 +2,14 @@ import express from "express";
 import { RegisterUser, UserLogin, UsersAllData } from "../controller/UserController.js";
 import { multerUpload } from "../middleware/multer.js";
 
-// console.log("Type of ------>", multerUpload.single("avatar"));
-
 const UserRouter = express.Router();
 
 // user all data API
 UserRouter.get("/user", UsersAllData);
 
-// const test = (req, res) => {
-//   console.log("This is the middleware ------>");
-// };
-//  UserRouter.post("/user", test, RegisterUser);
-
-// user Registration API
-
 UserRouter.post("/user", multerUpload.single("avatar"), RegisterUser);
 
 //User Login API
-UserRouter.get("/login", UserLogin);
-// UserRouter.post("/login", (req, res) => {
-//   res.status(200).send("The End Point is Working");
-// });
+UserRouter.post("/login", UserLogin);
 
 export default UserRouter;

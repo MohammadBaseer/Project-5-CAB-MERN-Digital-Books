3c49738daf19c2f51f8dc6b5df57efc6cc480e34
Baseer Rahimi <mohammadbaseer25@gmail.com>
2024-06-20 21:11:46 +0200

    import (setup) the primereact and primeflex

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.scss";
+import { PrimeReactProvider } from "primereact/api";
+import 'primereact/resources/themes/lara-light-indigo/theme.css'; //theme
+import 'primereact/resources/primereact.min.css'; //core css
+import 'primeicons/primeicons.css'; //icons
+import 'primeflex/primeflex.css'; // flex
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <PrimeReactProvider>
     <App />
+    </PrimeReactProvider>
   </React.StrictMode>
 );

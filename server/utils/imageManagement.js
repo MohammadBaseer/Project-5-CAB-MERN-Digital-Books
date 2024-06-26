dd729dc03225e18704cc3ba70276e09c7ffad0ca
Baseer Rahimi <mohammadbaseer25@gmail.com>
2024-06-26 23:18:12 +0200

    A function for delete the image from Cloudinary created

--- server/utils/imageManagement.js
+++ server/utils/imageManagement.js
@@ -1,6 +1,6 @@
 import { v2 as cloudinary } from "cloudinary";
 
-export const imageUpload = async (file, folder) => {
+const imageUpload = async (file, folder) => {
   try {
     const result = await cloudinary.uploader.upload(file.path, { folder: folder });
     // console.log(result);
@@ -9,3 +9,28 @@ export const imageUpload = async (file, folder) => {
     console.log(error);
   }
 };
+
+//! Remove the image from cloudinary
+
+const removeImage = async (cloudinaryImageFolder, imageUrl) => {
+  const getImageUrl = imageUrl;
+  const urlArray = getImageUrl.split("/");
+  const image = urlArray[urlArray.length - 1];
+  const cleanImageName = image.split(".")[0];
+  const imageFolder = cloudinaryImageFolder;
+
+  const imageName = `${imageFolder}/${cleanImageName}`;
+
+  try {
+    const result = await cloudinary.uploader.destroy(imageName);
+    if (result.result === "ok") {
+      console.log("Image deleted successfully:", result);
+    } else {
+      console.error("Error deleting image:", result);
+    }
+  } catch (error) {
+    console.error("Something went wrong", error);
+  }
+};
+
+export { imageUpload, removeImage };

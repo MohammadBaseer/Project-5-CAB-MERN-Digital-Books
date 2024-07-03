b86ae24a4bfb7067bc6633e44b4d21bdde05165e
Baseer Rahimi <mohammadbaseer25@gmail.com>
2024-07-03 13:36:00 +0200

    Home Page Designed

--- client/src/Components/Website/Pages/Home/Home.tsx
+++ client/src/Components/Website/Pages/Home/Home.tsx
@@ -1,7 +1,128 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./Home.module.scss";
+import "./css.css";
+
+import Loading from "../../../../Utils/Loading/Loading";
+import { useContext, useEffect, useState } from "react";
+import { FetchApiContext } from "../../../../Context/FetchApiContext";
 
 const Home = () => {
-  return <div></div>;
+  const { data, loading, errorHandle } = useContext(FetchApiContext);
+  const itemSlice = data?.slice(0, 27)
+
+//!
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+//!
+
+   const shuffled = data?.sort(() => 0.5 - Math.random());
+ var item = shuffled?.slice(0, 4);
+
+  return (
+    <>
+    <h1>{windowWidth}</h1>
+      <div className={styles.container}>
+        <br />
+        <div className={styles.slider}>
+          <img className={styles.slider_image} src="https://dispatch.barnesandnoble.com/content/dam/ccr/homepage/daily/2024/07/02/29800_Quote_A2_Evenings_and_Weekends_07_02_24.jpg" alt="" />
+        </div>
+        <div className={styles.random_items_box}>
+          <div className={`${styles.random_items_box_a}  col-12 md:col-12 lg:col-12`}>
+
+<div className={styles.books_list}>
+          {item &&
+            item.map((displayItem, index) => (
+              <div className={styles.books} key={index}>
+                <div className={styles.books_elements}>
+                  <div className={styles.book_image}>
+                    <img src={displayItem.image} alt="" />
+                  </div>
+                  <div className={styles.book_title}>
+                    <p className={styles.title}>
+                      <strong>{displayItem.title}</strong>
+                    </p>
+                    <p className={styles.author}>{displayItem?.detail?.publishAt}</p>
+                    <p className={styles.author}>{displayItem.authors[0]}</p>
+                    <p className={styles.star}></p>
+                    <a href="/read" className={styles.read}>
+                      <i className="pi pi-book"> Read</i>
+                    </a>
+                    {displayItem.detail && (
+                      <Link to={`${displayItem._id}`} className={styles.content}>
+                        <i className="pi pi-file"> Details</i>
+                      </Link>
+                    )}
+                  </div>
+                </div>
+              </div>
+            ))}
+        </div>
+
+        {loading && <Loading />}
+
+
+
+          </div>
+        </div>
+
+        <br />
+        <br />
+        <div className={styles.hr_line}></div>
+        <hr />
+        <br />
+        {/* //!================================================================================== */}
+
+        <div className={styles.books_list}>
+          {itemSlice &&
+            itemSlice.map((displayItem, index) => (
+              <div className={styles.books} key={index}>
+                <div className={styles.books_elements}>
+                  <div className={styles.book_image}>
+                    <img src={displayItem.image} alt="" />
+                  </div>
+                  <div className={styles.book_title}>
+                    <p className={styles.title}>
+                      <strong>{displayItem.title}</strong>
+                    </p>
+                    <p className={styles.author}>{displayItem?.detail?.publishAt}</p>
+                    <p className={styles.author}>{displayItem.authors[0]}</p>
+                    <p className={styles.star}></p>
+                    <a href="/read" className={styles.read}>
+                      <i className="pi pi-book"> Read</i>
+                    </a>
+                    {displayItem.detail && (
+                      <Link to={`${displayItem._id}`} className={styles.content}>
+                        <i className="pi pi-file"> Details</i>
+                      </Link>
+                    )}
+                  </div>
+                </div>
+              </div>
+            ))}
+        </div>
+        {
+          <div className={styles.error}>
+            <h2>{errorHandle}</h2>
+          </div>
+        }
+        {loading && <Loading />}
+
+        <br />
+        <br />
+        <br />
+      </div>
+    </>
+  );
 };
 
 export default Home;

5b635b1331c149993ff2009d547aa4786dc6b42e
Baseer Rahimi <mohammadbaseer25@gmail.com>
2024-07-06 22:12:39 +0200

    Modified

--- client/src/@Types/Types.ts
+++ client/src/@Types/Types.ts
@@ -1,17 +1,60 @@
+///REVIEW -  Restructured ---
+
+export type BooksDataType = {
+  _id: string;
+  title: string;
+  image: string;
+  authors: string[];
+  userRef: string;
+  likes: string[];
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
+  detail: Detail;
+  comment: CommentsType[];
+  id: string;
+};
+
+export type Detail = {
+  _id: string;
+  longDescription: string;
+  categories: string[];
+  publishAt: string;
+  bookref: string;
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
+};
+
+export type CommentsType = {
+  _id: string;
+  comment: string;
+  bookRef: string;
+  userRef: string;
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
+  users: User[];
+  id: string;
+};
+
+export type User = {
+  id: string;
+  name: string;
+  email: string;
+  avatar: string;
+  createdAt: string;
+  surname: string;
+  dob: string;
+  address: string;
+};
+
+//! ---------------------------
+
 //! USer Reiteration Types
 export type NotOkType = {
   error: string;
   message: string;
-};
-
-export type User = {
-  email: string;
-  name: string;
-  id: string;
-  avatar: string;
-  surname: string;
-  dob: any;
-  address: string
 };
 
 export type LoginOkResponse = {
@@ -21,42 +64,6 @@ export type LoginOkResponse = {
   email: string;
   error: NotOkType;
 };
-
-//! Books Data Type
-export type CommentType = {
-  id: string;
-  createdAt: any;
-  comment: string;
-  bookRef: string;
-  userRef: string;
-  users: User[];
-};
-export declare type BooksDataType = {
-  totalResult: number;
-  allBooks: Array<{
-    _id: string;
-    title: string;
-    image: string;
-    authors: string[];
-    detail: {
-      publishAt: any;
-      longDescription: string;
-      categories: string[];
-    };
-    comment: CommentType[];
-  }>;
-};
-
-//! Comments Data Type
-export declare type CommentsType = {
-  _id: string;
-  comments: Array<string>;
-  userRef: string;
-  bookRef: string;
-};
-
-//! User Profile Types
-
 export type GetProfileResponse = {
   msg: string;
   user: User;

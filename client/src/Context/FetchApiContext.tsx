6f06d53d9c592ea28ad764c9e74a5faaa40bd267
Baseer Rahimi <mohammadbaseer25@gmail.com>
2024-07-06 23:44:32 +0200

    added Like / unlike function

--- client/src/Context/FetchApiContext.tsx
+++ client/src/Context/FetchApiContext.tsx
@@ -1,16 +1,23 @@
-import { ReactNode, createContext, useEffect, useState } from "react";
+import { ReactNode, createContext, useEffect, useRef, useState } from "react";
 import { BooksDataType } from "../@Types/Types";
+import { isToken } from "../Utils/tokenServices";
+import { Toast } from "primereact/toast";
+import { BaseURL } from "../Utils/URLs/ApiURL";
 
 type FetchApiContextType = {
   data: BooksDataType[] | null;
   ApiFetchDataFun: () => void;
   loading: boolean;
   errorHandle: string;
+  likeFunction: (bookID: string) => Promise<void>;
 };
 
 const initFetchApiContext = {
   data: [] as BooksDataType[] | null,
   ApiFetchDataFun: () => {
+    throw new Error("The ApiFetchDataFun Error");
+  },
+  likeFunction: () => {
     throw new Error("The ApiFetchDataFun Error");
   },
   loading: true,
@@ -26,10 +33,10 @@ const FetchApiContextProvider = ({ children }: FetchApiContextProvider) => {
   const [loading, setLoading] = useState(true);
   const [errorHandle, setErrorHandle] = useState("");
 
-  const ApiURL = "http://localhost:5000/api/books";
+  //! Fetch the Books Data
   const ApiFetchDataFun = async () => {
     try {
-      const url = await fetch(ApiURL);
+      const url = await fetch(`${BaseURL}/api/books`);
       const result = (await url.json()) as BooksDataType[];
       console.log("FetchApiContext Test ====>", result);
       setData(result);
@@ -44,7 +51,54 @@ const FetchApiContextProvider = ({ children }: FetchApiContextProvider) => {
     ApiFetchDataFun();
   }, []);
 
-  return <FetchApiContext.Provider value={{ data, ApiFetchDataFun, loading, errorHandle }}> {children}</FetchApiContext.Provider>;
+  const toast = useRef<Toast>(null);
+
+  //! Like & unLike Function
+  const likeFunction = async (bookID: string) => {
+    const token = localStorage.getItem("token");
+    const isUserLogged = isToken();
+
+    if (!isUserLogged) {
+      toast.current?.show({ severity: "error", summary: "Error", detail: "Please Login!", life: 3000 });
+      return;
+    }
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+    myHeaders.append("Authorization", `Bearer ${token}`);
+    const urlencoded = new URLSearchParams();
+    urlencoded.append("bookId", bookID);
+    const requestOptions = {
+      method: "PUT",
+      headers: myHeaders,
+      body: urlencoded,
+    };
+
+    try {
+      const response = await fetch(`${BaseURL}/api/likes`, requestOptions);
+      if (response.ok) {
+        const result = await response.json();
+        ApiFetchDataFun();
+
+        let msg: "error" | "success" | "info" | "warn" | undefined = "info";
+
+        if (result.error === "Like") {
+          msg = "success";
+        } else if (result.error === "unLike") {
+          msg = "error";
+        }
+        toast.current?.show({ severity: msg, summary: "Success", detail: result.error, life: 3000 });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <FetchApiContext.Provider value={{ data, ApiFetchDataFun, loading, errorHandle, likeFunction }}>
+      {children}
+      <Toast ref={toast} />;
+    </FetchApiContext.Provider>
+  );
 };
 
 export default FetchApiContextProvider;

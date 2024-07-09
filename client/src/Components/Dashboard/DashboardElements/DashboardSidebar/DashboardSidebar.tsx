cbe963222ee1bf779dec6e46fd4aef5476d787a0
Baseer Rahimi <mohammadbaseer25@gmail.com>
2024-07-09 23:34:50 +0200

    re designed

--- client/src/Components/Dashboard/DashboardElements/DashboardSidebar/DashboardSidebar.tsx
+++ client/src/Components/Dashboard/DashboardElements/DashboardSidebar/DashboardSidebar.tsx
@@ -6,51 +6,50 @@ const DashboardSidebar = () => {
   return (
     <div className={styles.main_container}>
       <div className={styles.navbar}>
-        <h1>Sidebar</h1>
+        <h1>Admin Panel</h1>
         <div className={styles.nav_elements}>
           <ul className={styles.ul_main}>
             <li className={styles.li_main}>
-              <NavLink to="dashboard">
-                <span className="pi pi-home"> Dashboard</span>
-              </NavLink>
+              <span className="pi pi-home">&nbsp;Dashboard</span>
             </li>
+            <ul className={styles.ul_sub}>
+              <li className={styles.li_sub}>
+                <NavLink to="dashboard">
+                  <span className="pi pi-angle-right">&nbsp;Home</span>
+                </NavLink>
+              </li>
+            </ul>
             <li className={styles.li_main}>
-              <NavLink to="">
-                <span className="pi pi-book"> Book Section</span>
-              </NavLink>
+              <span className="pi pi-book">&nbsp;Book Section</span>
             </li>
             <ul className={styles.ul_sub}>
               <li className={styles.li_sub}>
                 <NavLink to="books-table">
-                  <span className="pi pi-angle-right"> Books</span>
+                  <span className="pi pi-angle-right">&nbsp;Books</span>
                 </NavLink>
               </li>
             </ul>
 
             <li className={styles.li_main}>
-              <NavLink to="">
-                <span className="pi pi-cog"> Web Contact Section </span>
-              </NavLink>
+              <span className="pi pi-cog">&nbsp;Web Contact Section </span>
             </li>
 
             <ul className={styles.ul_sub}>
               <li className={styles.li_sub}>
                 <NavLink to="/messages">
-                  <span className="pi pi-envelope"> Messages</span>
+                  <span className="pi pi-angle-right">&nbsp;Messages</span>
                 </NavLink>
               </li>
             </ul>
 
             <li className={styles.li_main}>
-              <NavLink to="">
-                <span className="pi pi-cog"> Settings</span>
-              </NavLink>
+              <span className="pi pi-cog">&nbsp;Settings</span>
             </li>
 
             <ul className={styles.ul_sub}>
               <li className={styles.li_sub}>
                 <NavLink to="/users">
-                  <span className="pi pi-user"> Users</span>
+                  <span className="pi pi-angle-right">&nbsp;Users</span>
                 </NavLink>
               </li>
             </ul>
